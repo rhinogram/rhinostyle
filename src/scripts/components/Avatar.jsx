bad2126f876782cb62c6db521e58df00c6fe41ed
Ian Greulich <ian@greulich.me>
2016-06-02 12:14:17 -0400

    Added images to avatar

--- src/scripts/components/Avatar.jsx
+++ src/scripts/components/Avatar.jsx
@@ -5,8 +5,9 @@ class Avatar extends React.Component {
   static displayName = 'RhinoAvatar';
 
   static propTypes = {
-    size: React.PropTypes.oneOf(['small', 'default', 'large']),
-    type: React.PropTypes.oneOf(['member', 'patient']).isRequired
+    image: React.PropTypes.string,
+    size:  React.PropTypes.oneOf(['small', 'default', 'large']),
+    type:  React.PropTypes.oneOf(['member', 'patient']).isRequired
   };
 
   static defaultProps = {
@@ -14,16 +15,19 @@ class Avatar extends React.Component {
   };
 
   render() {
-    const { size, type } = this.props;
+    const { size, type, image } = this.props;
     const cx = classNames('avatar', {
       'avatar--sm': size==='small',
       'avatar--lg': size==='large',
       'avatar--member':  type==='member',
       'avatar--patient': type==='patient'
     });
+    const styles = {
+      backgroundImage: `url(${image})`
+    }
 
     return (
-      <figure className={cx}>{this.props.children}</figure>
+      <figure className={cx} style={styles}>{this.props.children}</figure>
     );
   }
 }
