089fd35b88cb2603e7109bd49069b47a10470e5f
Ian Greulich <ian@greulich.me>
2016-06-07 10:24:57 -0400

    toast can have icon, and text only

--- src/scripts/components/Toast.jsx
+++ src/scripts/components/Toast.jsx
@@ -1,31 +1,42 @@
 import React      from 'react';
 import classNames from 'classnames';
 
+import Icon from './Icon';
+
 class Toast extends React.Component {
   static displayName = 'RhinoToast';
 
   static propTypes = {
-    body:      React.PropTypes.oneOfType([React.PropTypes.string, React.PropTypes.element]).isRequired,
+    body:      React.PropTypes.string.isRequired,
+    icon:      React.PropTypes.string,
     onDismiss: React.PropTypes.func,
     type:      React.PropTypes.oneOf(['danger', 'default', 'secondary'])
   };
 
   static defaultProps = {
+    icon:      '',
     onDismiss: () => {},
-    type:      'default'
+    type:      'default',
   };
 
   render() {
-    const { body, onDismiss, type } = this.props;
+    const { body, icon, onDismiss, type } = this.props;
     const cx = classNames('toast', {
       'toast--danger':    type==='danger',
       'toast--default':   type==='default',
       'toast--secondary': type==='secondary'
     });
 
+    const renderIcon = () => {
+      if (icon) {
+        return (<Icon icon={icon} />);
+      }
+    }
+
     return (
       <div className={cx}>
         <button type="button" onClick={onDismiss} className="toast__close" data-dismiss="toast" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        {renderIcon()}
         {body}
       </div>
     );
