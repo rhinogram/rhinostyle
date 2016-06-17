594788206d54b60c28012361d2c39614ce254dec
Ben Bruning <bruning@gmail.com>
2016-06-17 16:36:05 -0400

    dropdown work

--- src/scripts/components/DropdownMenuItem.jsx
+++ src/scripts/components/DropdownMenuItem.jsx
@@ -10,6 +10,7 @@ class DropdownMenuItem extends React.Component {
     click:     React.PropTypes.func,
     className: React.PropTypes.string,
     disabled:  React.PropTypes.bool,
+    url:       React.PropTypes.string,
   };
 
   static defaultProps = {
@@ -18,15 +19,27 @@ class DropdownMenuItem extends React.Component {
     disabled: false,
   };
 
+  handleClick = () => {
+    let returnVal = null;
+    if (this.props.url) {
+      window.location = this.props.url;
+      returnVal = null;
+    } else {
+      returnVal = this.props.click();
+    }
+
+    return returnVal;
+  }
+
   render() {
-    const { active, className, click, disabled } = this.props;
+    const { active, className, disabled } = this.props;
     const classes = cx('dropdown__menu__item', className, {
       'active': active, //eslint-disable-line
       'disabled': disabled, //eslint-disable-line
     });
 
     return (
-      <li className={classes}><a href="javascript:void(0)" onClick={click}>{this.props.children}</a></li>/* eslint no-script-url:0 */
+      <li className={classes}><a href="javascript:void(0)" onClick={this.handleClick}>{this.props.children}</a></li>/* eslint no-script-url:0 */
     );
   }
 }
