4b736deead3e3436d3c6b845a52d44ae5f53563a
Ian Greulich <ian@greulich.me>
2016-06-21 16:54:16 -0400

    finish input add-ons, add to docs

--- src/scripts/components/Input.jsx
+++ src/scripts/components/Input.jsx
@@ -5,8 +5,7 @@ class Input extends React.Component {
   static displayName = 'RhinoInput';
 
   static propTypes = {
-    addonLeft:   React.PropTypes.string,
-    addonRight:  React.PropTypes.string,
+    addon:       React.PropTypes.oneOf(['', 'left', 'right', 'both']),
     label:       React.PropTypes.string,
     name:        React.PropTypes.string,
     placeholder: React.PropTypes.string,
@@ -15,6 +14,7 @@ class Input extends React.Component {
   };
 
   static defaultProps = {
+    addon: '',
     label: '',
     name:  '',
     type:  'text',
@@ -29,20 +29,60 @@ class Input extends React.Component {
   }
 
   render() {
-    const { label, name, placeholder, type, value } = this.props;
+    const { addon, label, name, placeholder, type, value } = this.props;
     const classes = cx('form__control');
 
     const showLabel = () => {
       if (label) {
-        return (<label htmlFor={name}>{label}</label>);
+        return <label htmlFor={name}>{label}</label>;
       }
+
       return false;
+    };
+
+    const showInput = () => {
+      switch (addon) {
+        case 'left':
+          return (
+            <div className="form__addon">
+              <div className="form__addon__item form__addon__item--left">
+                {/* eslint react/prop-types:0 */}
+                {this.props.children}
+              </div>
+              <input value={value} type={type} className={classes} id={name} placeholder={placeholder} ref="input" />
+            </div>
+          );
+        case 'right':
+          return (
+            <div className="form__addon">
+              <input value={value} type={type} className={classes} id={name} placeholder={placeholder} ref="input" />
+              <div className="form__addon__item form__addon__item--right">
+                {this.props.children}
+              </div>
+            </div>
+          );
+        case 'both':
+          return (
+            <div className="form__addon">
+              <div className="form__addon__item form__addon__item--left">
+                {this.props.children[0]}
+              </div>
+              <input value={value} type={type} className={classes} id={name} placeholder={placeholder} ref="input" />
+              <div className="form__addon__item form__addon__item--right">
+                {this.props.children[1]}
+              </div>
+            </div>
+          );
+        case '':
+        default:
+          return <input value={value} type={type} className={classes} id={name} placeholder={placeholder} ref="input" />;
+      }
     };
 
     return (
       <div className="form__group">
         {showLabel()}
-        <input value={value} type={type} className={classes} id={name} placeholder={placeholder} ref="input" />
+        {showInput()}
       </div>
     );
   }
