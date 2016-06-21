aa48f9d0da48881896ce70a2d3c9736276cfbe4f
Forrest Guilloud <fbguillo@gmail.com>
2016-06-21 12:32:53 -0400

    build

--- src/scripts/docs/NavigationApp.jsx
+++ src/scripts/docs/NavigationApp.jsx
@@ -16,13 +16,52 @@ const exampleScope  = {
   NavTabs,
   NavTabsItem,
 };
+class NavigationApp extends React.Component {
+  state = {
+    activeKey: 1,
+  };
 
-const NavigationApp = () =>
-  <div>
-    <section className="site-section">
-      <h3 className="site-subheadline">Tab Types</h3>
-      <Playground codeText={navigationExample} scope={exampleScope} noRender={false} />
-    </section>
-  </div>;
+  updateActiveKey = (index) => {
+    this.setState({
+      activeKey: index,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Tabs activeKey={this.state.activeKey} select={this.updateActiveKey}>
+          <NavTabs>
+            <NavTabsItem id={1}>Tab One</NavTabsItem>
+            <NavTabsItem id={2}>Tab Two</NavTabsItem>
+            <NavTabsItem id={3}>Tab Three</NavTabsItem>
+          </NavTabs>
+          <TabsContent>
+            <TabContentPane id={1}>Content 1 </TabContentPane>
+            <TabContentPane id={2}>Content 2</TabContentPane>
+            <TabContentPane id={3}>Content 3</TabContentPane>
+          </TabsContent>
+        </Tabs>
+        <div>
+          <section className="site-section">
+            <h3 className="site-subheadline">Tab Types</h3>
+            <Playground codeText={navigationExample} scope={exampleScope} noRender={false} />
+          </section>
+        </div>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<NavigationApp />, document.getElementById('js-app'));
+
+
+// const NavigationApp = () =>
+  // <div>
+  //   <section className="site-section">
+  //     <h3 className="site-subheadline">Tab Types</h3>
+  //     <Playground codeText={navigationExample} scope={exampleScope} noRender={false} />
+  //   </section>
+  // </div>;
+//
+// ReactDOM.render(<NavigationApp />, document.getElementById('js-app'));
