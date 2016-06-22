31ef3cb265c157e46b4983cec4f26f4e436520e4
Ben Bruning <bruning@gmail.com>
2016-06-22 16:09:15 -0400

    working, but ugly

--- src/scripts/components/DropdownSelectFilter.jsx
+++ src/scripts/components/DropdownSelectFilter.jsx
@@ -12,71 +12,60 @@ class DropdownSelectFilter extends React.Component {
   };
 
   static defaultProps = {
-    active:   false,
-    block:    false,
-    disabled: false,
     select:   () => {},
-    type:     'default',
-    wide:     false,
   };
 
   getChildren = () => {
-    const results = [];
-    const children = this.props.children;
+    // if (this.state && this.state.query) {
+    //   console.log('state', this.state);
+    //   debugger;
+    //   this.handleFilter(this.state.query);
+    // } else {
+      const results = [];
+      let children = this.props.children;
 
-    React.Children.forEach(children, child => {
-      if (child.type === DropdownMenuItem) {
-        results.push(React.cloneElement(child, {
-          click: () => this.props.select(child.props.id),
-          active: child.props.id === this.props.activeKey,
-          key: child.props.id,
-        }));
+      if (this.state && this.state.query) {
+        children = this.state.results;
       }
-    });
 
-    return results;
+      React.Children.forEach(children, child => {
+        if (child.type === DropdownMenuItem) {
+          results.push(React.cloneElement(child, {
+            click: () => this.props.select(child.props.id),
+            active: child.props.id === this.props.activeKey,
+            key: child.props.id,
+          }));
+        }
+      });
+
+      return results;
+    //}
   }
 
   state = {
+    query: '',
     results: this.getChildren(),
   };
 
   componentWillReceiveProps(nextProps) {
-    //if (nextProps.activeKey !== this.props.activeKey) {
-      // this.setState({
-      //   results: this.getChildren(),
-      // });
+  //  if (nextProps.activeKey !== this.props.activeKey) {
+      this.setState({
+        results: this.getChildren(),
+      });
 
-      //    console.log('receiving props', this.props)
-  //  }
+          console.log('receiving props', nextProps)
+    //}
   }
 
-  handleFilter = (e) => {
-    //const query = e.target.value;
-    // const results = [];
-    // const children = this.props.children;
-    //
-    // React.Children.forEach(children, child => {
-    //   if (child.type === DropdownMenuItem) {
-    //     const searchText = child.props.children;
-    //
-    //     if (searchText.toLowerCase().indexOf(query.toLowerCase()) !== -1) {
-    //       results.push(React.cloneElement(child, {
-    //         click: () => this.props.select(child.props.id),
-    //         active: child.props.id === this.props.activeKey,
-    //         key: child.props.id,
-    //       }));
-    //     }
-    //   }
-    // });
+  handleFilterChange = (e) => {
+    this.handleFilter(e.target.value);
+  }
 
-    const query = e.target.value;
+  handleFilter = (query) => {
     const results = [];
     const children = this.props.children;
 
-  //  this.state.results.forEach((child) => {
     React.Children.forEach(children, child => {
-      console.log('chillin', child);
       if (child.type === DropdownMenuItem) {
         const searchText = child.props.children;
 
@@ -90,16 +79,19 @@ class DropdownSelectFilter extends React.Component {
       }
     });
 
+console.log('handle filter', query, results);
     this.setState({
+      query,
       results,
     });
   }
 
   render() {
+    console.log(this.state.results)
     return (
       <div>
         <div className="dropdown__menu__container">
-          <input type="text" className="form__control" id="exampleInputDropdown" placeholder="Filter contact" onChange={this.handleFilter} />
+          <input type="text" className="form__control" id="exampleInputDropdown" placeholder="Filter contact" onChange={this.handleFilterChange} />
         </div>
         <DropdownMenuScroll>
           {this.state.results}
