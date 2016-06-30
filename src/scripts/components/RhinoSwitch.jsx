373761372205401ede31e81a44332244ad9fe547
Ian Greulich <ian@greulich.me>
2016-06-30 15:05:15 -0400

    rough in rhinswitch

---
+++ src/scripts/components/RhinoSwitch.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import cx    from 'classnames';
+
+class RhinoSwitch extends React.Component {
+  static display = 'RhinoSwitch';
+
+  static propTypes = {};
+
+  static defaultProps = {};
+
+  render() {
+    return (
+      <div className="form__group">
+      </div>
+    );
+  }
+}
+
+export default RhinoSwitch;
