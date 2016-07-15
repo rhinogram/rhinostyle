52f76c35b0f2de1c3c338f18e191bffeab7425a2
Ian Greulich <ian@greulich.me>
2016-07-15 10:47:52 -0400

    make webpack meet eslint rules

--- config/webpack.dist.config.js
+++ config/webpack.dist.config.js
@@ -4,33 +4,33 @@ import path    from 'path';
 export default {
   cache: true,
   entry: {
-    rhinostyle: path.join(__dirname, '../src/scripts/components/index.js')
+    rhinostyle: path.join(__dirname, '../src/scripts/components/index.js'),
   },
   output: {
     path: path.join(__dirname, '../dist/scripts'),
     filename: '[name].js',
     libraryTarget: 'umd',
-    library: 'Rhinostyle'
+    library: 'Rhinostyle',
   },
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
         exclude: [/node_modules/],
-        loader: 'babel-loader'
-      }
-    ]
+        loader: 'babel-loader',
+      },
+    ],
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['', '.js', '.jsx'],
   },
   plugins: [
     new webpack.optimize.UglifyJsPlugin({
-      sourceMap: false
-    })
+      sourceMap: false,
+    }),
   ],
   externals: [
     'react',
-    'classnames'
-  ]
-}
+    'classnames',
+  ],
+};
