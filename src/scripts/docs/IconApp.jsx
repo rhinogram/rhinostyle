49785332202a72197593dbc6cc8477064b79c934
Craig Anthony <mcraiganthony@gmail.com>
2016-08-24 09:36:59 -0400

    Craig - star icon

--- src/scripts/docs/IconApp.jsx
+++ src/scripts/docs/IconApp.jsx
@@ -264,6 +264,13 @@ const IconApp = () =>
 
       <div className="site-swatch">
         <div className="site-swatch__sample">
+          <Icon icon="star" />
+        </div>
+        <div className="site-swatch__text"><strong>Star</strong></div>
+      </div>
+
+      <div className="site-swatch">
+        <div className="site-swatch__sample">
           <Icon icon="tag" />
         </div>
         <div className="site-swatch__text"><strong>Tag</strong></div>
