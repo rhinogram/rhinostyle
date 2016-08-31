7df8d159652896a66cb33f7253a06d8b0396ff8f
Ben Bruning <bruning@gmail.com>
2016-08-31 15:46:59 -0400

    moved radio.jsx back to functional component

--- src/scripts/components/Radio.jsx
+++ src/scripts/components/Radio.jsx
@@ -1,41 +1,40 @@
 import React from 'react';
 import cx    from 'classnames';
 
-class Radio extends React.Component {
-  static displayName = 'Rhinodio';
+const Radio = (props) => {
+  const { className, inline, name, onChange, value } = props;
+  const id = `${name}-${Math.floor(Math.random() * 1000000)}`;
+  const classes = cx('rhinodio', className, {
+    'rhinodio--inline': inline,
+  });
 
-  static propTypes = {
-    children:      React.PropTypes.node,
-    className:     React.PropTypes.string,
-    inline:        React.PropTypes.bool,
-    name:          React.PropTypes.string,
-    onChange:      React.PropTypes.func,
-    selectedValue: React.PropTypes.oneOfType([React.PropTypes.string, React.PropTypes.number]),
-    value:         React.PropTypes.oneOfType([React.PropTypes.string, React.PropTypes.number]).isRequired,
-  };
+  return (
+    <div className={classes}>
+      <input type="radio" name={name} value={value} id={id} checked={props.value === props.selectedValue} onChange={onChange} />
+      <label htmlFor={id}>
+        {props.children}
+      </label>
+    </div>
+  );
+};
 
-  static defaultProps = {
-    inline: false,
-    onChange: () => {},
-  };
+Radio.displayName = 'Rhinodio';
 
-  render() {
-    const props = this.props;
-    const { className, inline, name, onChange, value } = props;
-    const id = `${name}-${Math.floor(Math.random() * 1000000)}`;
-    const classes = cx('rhinodio', className, {
-      'rhinodio--inline': inline,
-    });
+Radio.propTypes = {
+  children:      React.PropTypes.node,
+  className:     React.PropTypes.string,
+  inline:        React.PropTypes.bool,
+  name:          React.PropTypes.string,
+  onChange:      React.PropTypes.func,
+  selectedValue: React.PropTypes.oneOfType([React.PropTypes.string, React.PropTypes.number]),
+  value:         React.PropTypes.oneOfType([React.PropTypes.string, React.PropTypes.number]).isRequired,
+};
 
-    return (
-      <div className={classes}>
-        <input type="radio" name={name} value={value} id={id} checked={props.value === props.selectedValue} onChange={onChange} />
-        <label htmlFor={id}>
-          {props.children}
-        </label>
-      </div>
-    );
-  }
-}
+Radio.defaultProps = {
+  inline: false,
+  onChange() {
+    return true;
+  },
+};
 
 export default Radio;
