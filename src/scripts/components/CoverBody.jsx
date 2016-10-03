c1550cca65ee6019859052d0906ab92bdca4c488
Craig Anthony <mcraiganthony@gmail.com>
2016-10-03 09:41:25 -0400

    Craig - add className prop to CoverBody

--- src/scripts/components/CoverBody.jsx
+++ src/scripts/components/CoverBody.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import cx    from 'classnames';
 
 const CoverBody = (props) => {
-  const { children, size } = props;
+  const { children, className, size } = props;
 
-  const containerClasses = cx('cover__body__container', {
+  const containerClasses = cx('cover__body__container', className, {
     'cover__body__container--sm': !size || size === 'sm',
     'cover__body__container--md': size === 'md',
     'cover__body__container--lg': size === 'lg',
@@ -12,13 +12,14 @@ const CoverBody = (props) => {
 
   return (
     <div className="cover__body"><div className={containerClasses}>{children}</div></div>
-  )
+  );
 };
 
 CoverBody.displayName = 'RhinoCoverBody';
 
 CoverBody.propTypes = {
   children:   React.PropTypes.node,
+  className:  React.PropTypes.string,
   size:       React.PropTypes.string,
 };
 
