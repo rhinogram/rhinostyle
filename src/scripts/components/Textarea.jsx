5a2c9d33bdef6afb761df223ee2d23dec6086516
Craig Anthony <mcraiganthony@gmail.com>
2016-11-04 14:43:19 -0400

    Craig - character count on textarea

--- src/scripts/components/Textarea.jsx
+++ src/scripts/components/Textarea.jsx
@@ -9,6 +9,7 @@ class Textarea extends React.Component {
     explanationMessage: React.PropTypes.string,
     initialValue:       React.PropTypes.string,
     label:              React.PropTypes.string,
+    maxCharacters:      React.PropTypes.number,
     name:               React.PropTypes.string,
     onChange:           React.PropTypes.func,
     placeholder:        React.PropTypes.string,
@@ -17,14 +18,16 @@ class Textarea extends React.Component {
   };
 
   static defaultProps = {
-    label:       '',
-    name:        '',
-    placeholder: '',
-    required:    false,
-    rows:        3,
+    label:         '',
+    maxCharacters: null,
+    name:          '',
+    placeholder:   '',
+    required:      false,
+    rows:          3,
   };
 
   state = {
+    charactersLeft: '',
     value: '',
   };
 
@@ -32,10 +35,20 @@ class Textarea extends React.Component {
     if (this.props.initialValue) {
       this.setState({ value: this.props.initialValue });
     }
+
+    if (this.props.maxCharacters && this.props.initialValue) {
+      this.setState({ charactersLeft: this.props.maxCharacters - this.props.initialValue.length });
+    } else if (this.props.maxCharacters) {
+      this.setState({ charactersLeft: this.props.maxCharacters });
+    }
   }
 
   _handleChange = (event) => {
-    this.setState({ value: event.target.value });
+    this.setState({
+      value: event.target.value,
+    });
+
+    this.setState({ charactersLeft: this.props.maxCharacters - event.target.value.length });
 
     if (this.props.onChange && typeof(this.props.onChange === 'function')) {
       this.props.onChange(event.target.id, event.target.value);
@@ -43,9 +56,12 @@ class Textarea extends React.Component {
   }
 
   render() {
-    const { className, explanationMessage, label, name, placeholder, required, rows } = this.props;
+    const { className, explanationMessage, label, maxCharacters, name, placeholder, required, rows } = this.props;
     const textAreaClasses = cx('form__control');
     const formGroupClasses = cx('form__group', className);
+    const characterCountClasses = cx('form__character-count', {
+      'form__character-count--danger': this.state.charactersLeft < 11,
+    });
 
     const showExplanationMessage = () => {
       if (explanationMessage) {
@@ -63,11 +79,22 @@ class Textarea extends React.Component {
       return false;
     };
 
+    const showCharacterCount = () => {
+      if (maxCharacters) {
+        return <div className={characterCountClasses}>{this.state.charactersLeft} characters left</div>;
+      }
+
+      return false;
+    };
+
     return (
       <div className={formGroupClasses}>
         {showLabel()}
         <textarea id={name} className={textAreaClasses} rows={rows} placeholder={placeholder} value={this.state.value} onChange={this._handleChange}></textarea>
-        {showExplanationMessage()}
+        <div className="form__control-footer">
+          {showExplanationMessage()}
+          {showCharacterCount()}
+        </div>
       </div>
     );
   }
