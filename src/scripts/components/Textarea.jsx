86e6c847971f46d07f034fbf520455356981cf25
Forrest Guilloud <fbguillo@gmail.com>
2017-01-31 15:54:10 -0500

    removing console log

--- src/scripts/components/Textarea.jsx
+++ src/scripts/components/Textarea.jsx
@@ -62,7 +62,6 @@ class Textarea extends React.Component {
     this.setState({ charactersLeft: this.props.maxCharacters - event.target.value.length });
 
     if (this.props.onChange && typeof (this.props.onChange === 'function')) {
-      console.log('changing', event.target.value.trim());
       this.props.onChange(event.target.id, event.target.value.trim());
     }
   }
