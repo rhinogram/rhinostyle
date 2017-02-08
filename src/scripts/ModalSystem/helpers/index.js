62e077ecd297f0f5f99d45a9f1f79faccd46667b
Zach Schnackel <zachschnackel@gmail.com>
2017-02-08 15:11:25 -0500

    init work

--- src/scripts/ModalSystem/helpers/index.js
+++ src/scripts/ModalSystem/helpers/index.js
@@ -3,33 +3,20 @@ import ReactDOM           from 'react-dom';
 import { TweenMax, Expo } from 'gsap';
 
 const modalContainer  = document.createElement('div');
+modalContainer.setAttribute('data-js', 'modalContainer');
 
 /* do not render to body > https://medium.com/@dan_abramov/two-weird-tricks-that-fix-react-7cf9bbdef375#.jouodrjb5 */
 export function addModal(modal) {
   document.body.insertBefore(modalContainer, document.body.childNodes[0]);
-  document.body.classList.add('modal-open');
-  ReactDOM.render(<div>{modal}</div>, modalContainer);
+  ReactDOM.render({ modal }, modalContainer);
+
+  modal.modal.timeline.play();
 }
 
 export function refreshModal(modal) {
-  ReactDOM.render(<div>{modal}</div>, modalContainer);
+  ReactDOM.render({ modal }, modalContainer);
 }
 
-export function removeModal() {
-  document.body.classList.remove('modal-open');
-
-  TweenMax.to('.modal-backdrop', 0.35, {
-    opacity: 0,
-    ease: Expo.easeOut,
-  });
-
-  TweenMax.to('#rhino-modal', 0.35, {
-    scale: 0.98,
-    opacity: 0,
-    ease: Expo.easeOut,
-    onComplete: () => {
-      ReactDOM.unmountComponentAtNode(modalContainer);
-      document.body.removeChild(modalContainer);
-    },
-  });
+export function removeModal(modal) {
+  modal.modal.timeline.reverse();
 }
--- src/scripts/components/Modal.jsx
+++ src/scripts/components/Modal.jsx
@@ -1,7 +1,8 @@
 /* eslint no-return-assign:0 */
 import cx                 from 'classnames';
 import React              from 'react';
-import { TweenMax, Expo } from 'gsap';
+import ReactDOM           from 'react-dom';
+import { TimelineMax, Expo } from 'gsap';
 
 class Modal extends React.Component {
   static displayName = 'RhinoModal';
@@ -13,43 +14,71 @@ class Modal extends React.Component {
   };
 
   componentDidMount() {
-    TweenMax.set('.modal-backdrop', {
-      opacity: 0,
+    const $body = document.body;
+    const $modal = this.modal;
+    const $modalContainer = document.querySelector('[data-js="modalContainer"]');
+
+    let forward = true;
+    let lastTime = 0;
+
+    // Attach timeline to each instance
+    $modal.timeline = new TimelineMax({
+      paused: true,
+      onStart() {
+        $body.classList.add('modal-open');
+        console.log('started');
+      },
+      onUpdate: () => {
+        const newTime = $modal.timeline.time();
+        if ((forward && newTime < lastTime) || (!forward && newTime > lastTime)) {
+          forward = !forward;
+          if (!forward) {
+            $body.classList.remove('modal-open');
+            $modal.classList.remove('is-open');
+          }
+        }
+        lastTime = newTime;
+      },
+      onComplete() {
+        // Focus on active modal for screen readers
+        $modal.focus();
+      },
+      onReverseComplete() {
+        ReactDOM.unmountComponentAtNode($modalContainer);
+        $body.removeChild($modalContainer);
+        console.log('reverse completed');
+      },
     });
 
-    TweenMax.set('#rhino-modal', {
-      opacity: 0,
-      scale: 0.98,
-    });
-
-    TweenMax.to('.modal-backdrop', 0.35, {
-      opacity: 0.5,
-      ease: Expo.easeOut,
-    });
-
-    TweenMax.to('#rhino-modal', 0.35, {
-      scale: 1,
-      opacity: 1,
-      ease: Expo.easeOut,
+    $modal.timeline
+    .to($modal, 0.25, {
+      css: {
+        opacity: 1,
+        display: 'block',
+      },
+    })
+    .to($modal.querySelector('.modal__dialog'), 0.5, {
+      css: {
+        opacity: 1,
+        y: 0,
+        scale: 1,
+      },
+      ease: Expo.easeInOut,
     });
   }
 
   render() {
     const { children, className, size } = this.props;
-    const modalClasses     = cx('modal', className);
-    const containerClasses = cx('modal__container', {
-      'modal__container--sm': size === 'sm',
-      'modal__container--lg': size === 'lg',
-    });
+    const modalClasses     = cx('modal__dialog', {
+      'modal__dialog--sm': size === 'sm',
+      'modal__dialog--lg': size === 'lg',
+    }, className);
 
     return (
-      <div>
-        <div className="modal-backdrop" />
-        <div id="rhino-modal" className={modalClasses}>
-          <div className={containerClasses}>
-            <div className="modal__content">
-              {children}
-            </div>
+      <div className="modal" ref={ref => this.modal = ref}>
+        <div className={modalClasses}>
+          <div className="modal__content">
+            {children}
           </div>
         </div>
       </div>
