4f49fdb4b9fa7236b6e4e79af5df76492c8b3572
Zach Schnackel <zachschnackel@gmail.com>
2017-02-15 14:41:27 -0500

    fix cover/modal render function

--- src/scripts/CoverSystem/helpers/index.js
+++ src/scripts/CoverSystem/helpers/index.js
@@ -1,4 +1,5 @@
-import ReactDOM           from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 const $coverContainer  = document.createElement('div');
 $coverContainer.setAttribute('data-js', 'coverContainer');
@@ -6,13 +7,13 @@ $coverContainer.setAttribute('data-js', 'coverContainer');
 /* do not render to body > https://medium.com/@dan_abramov/two-weird-tricks-that-fix-react-7cf9bbdef375#.jouodrjb5 */
 export function addCover(cover) {
   document.body.insertBefore($coverContainer, document.body.childNodes[0]);
-  ReactDOM.render(cover, $coverContainer);
+  ReactDOM.render(<div>{ cover }</div>, $coverContainer);
 
   $coverContainer.querySelector('.cover').timeline.play();
 }
 
 export function refreshCover(cover) {
-  ReactDOM.render({ cover }, $coverContainer);
+  ReactDOM.render(<div>{ cover }</div>, $coverContainer);
 }
 
 export function removeCover() {
--- src/scripts/ModalSystem/helpers/index.js
+++ src/scripts/ModalSystem/helpers/index.js
@@ -1,4 +1,5 @@
-import ReactDOM           from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 const $modalContainer  = document.createElement('div');
 $modalContainer.setAttribute('data-js', 'modalContainer');
@@ -6,13 +7,13 @@ $modalContainer.setAttribute('data-js', 'modalContainer');
 /* do not render to body > https://medium.com/@dan_abramov/two-weird-tricks-that-fix-react-7cf9bbdef375#.jouodrjb5 */
 export function addModal(modal) {
   document.body.insertBefore($modalContainer, document.body.childNodes[0]);
-  ReactDOM.render(modal, $modalContainer);
+  ReactDOM.render(<div>{modal}</div>, $modalContainer);
 
   $modalContainer.querySelector('.modal').timeline.play();
 }
 
 export function refreshModal(modal) {
-  ReactDOM.render({ modal }, $modalContainer);
+  ReactDOM.render(<div>{modal}</div>, $modalContainer);
 }
 
 export function removeModal() {
