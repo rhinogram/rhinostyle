56ca0ca56d9c67b82905424d691fcc4e3089d4a0
Zach Schnackel <zachschnackel@gmail.com>
2017-02-27 13:19:56 -0500

    doc note

--- src/scripts/docs/LightboxApp.jsx
+++ src/scripts/docs/LightboxApp.jsx
@@ -22,6 +22,8 @@ const LightboxApp = () =>
   <div>
     <h1 className="site-headline">Lightbox</h1>
 
+    <p className="site-copy">We are using <a href="https://github.com/fritz-c/react-image-lightbox" target="_blank" rel="noopener noreferrer">react-image-lightbox</a> for lightbox images/galleries.</p>
+
     <section className="site-section">
       <h3 className="site-subheadline">Lightbox Playground</h3>
       <Playground docClass={Lightbox} propDescriptionMap={lightboxDocs} codeText={lightboxExample} scope={lightboxScope} noRender={false} />
