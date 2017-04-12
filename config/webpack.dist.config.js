58d1fe7c0eb5cb8de326c3300ec3aff0482c6c58
Zach Schnackel <zachschnackel@gmail.com>
2017-04-12 16:32:35 -0400

    enable eslint in dist + remove deprecated .eslint rule

--- config/webpack.dist.config.js
+++ config/webpack.dist.config.js
@@ -23,7 +23,7 @@ module.exports = {
         exclude: [/node_modules/],
         use: [
           'babel-loader',
-          //'eslint-loader',
+          'eslint-loader',
         ],
       },
     ],
