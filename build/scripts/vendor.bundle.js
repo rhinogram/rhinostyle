fed82e1a7b634e8c407887bffddd759e9698c223
Zach Schnackel <zachschnackel@gmail.com>
2017-07-05 09:50:40 -0400

    Update GSAP to 1.20.2

--- build/scripts/vendor.bundle.js
+++ build/scripts/vendor.bundle.js
@@ -150,7 +150,7 @@
 /******/ 	__webpack_require__.oe = function(err) { console.error(err); throw err; };
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 839);
+/******/ 	return __webpack_require__(__webpack_require__.s = 838);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -123959,8 +123959,7 @@ module.exports = traverseAllChildren;
 /***/ }),
 /* 836 */,
 /* 837 */,
-/* 838 */,
-/* 839 */
+/* 838 */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(2);
