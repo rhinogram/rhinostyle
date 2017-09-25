e875720ac019dbcc4f521d9f7b39946c7694993e
Zach Schnackel <info@zslabs.com>
2017-09-25 16:42:24 -0400

    Add tooltip delay (#89)
    * Add tooltip delay
    
    * remove unneeded number conversion

--- src/scripts/docs/TooltipsApp.jsx
+++ src/scripts/docs/TooltipsApp.jsx
@@ -17,13 +17,14 @@ const tooltipDocs = {
   children: 'Only accepts one child',
   placement: 'Position of tooltip - [top | right | bottom | left]',
   content: 'Content of tooltip. May contain HTML or other components',
+  delay: 'Delay showing the tooltip onmouseenter. Can be either the prop itself (defaults 1000 milliseconds) or you can pass in a value',
 };
 
 const TooltipsApp = () =>
   (<div>
     <section className="site-section">
       <h3 className="site-subheadline">About Tooltips</h3>
-      <p className="site-text-lead">Our tooltips can be attached to any valid element, including other React components. For desktop, a tooltip is initiated on <code>mouseenter</code> and <code>mouseleave</code> of the trigger. For touch, the trigger is used as a toggle on <code>click</code>; so an actionable item should not be used in conjunction with this component.</p>
+      <p className="site-text-lead">Our tooltips can be attached to any valid element, including other React components. For desktop, a tooltip is initiated on <code>mouseenter</code> and <code>mouseleave</code> of the trigger. The component interaction is disabled on touch devices due to a lack of hover state.</p>
     </section>
 
     <section className="site-section">
