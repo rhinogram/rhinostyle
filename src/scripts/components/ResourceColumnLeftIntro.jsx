adfbfe9969e4e34f6808176402085704af6aa0e9
Zach Schnackel <zachschnackel@gmail.com>
2017-11-10 16:38:02 -0500

    more options for resources

--- src/scripts/components/ResourceColumnLeftIntro.jsx
+++ src/scripts/components/ResourceColumnLeftIntro.jsx
@@ -1,17 +1,67 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import { Avatar, Icon } from '../components';
+
 class ResourceColumnLeftIntro extends React.Component {
   componentDidMount() {
     // console.log();
   }
 
-  render() {
-    const { title } = this.props;
+  handleIconClick = () => {
+    if (this.props.iconClick && typeof (this.props.iconClick === 'function')) {
+      this.props.iconClick();
+    }
+  }
+
+  renderMedia = () => {
+    const { icon, avatar } = this.props;
+    let output = null;
+
+    if (icon) {
+      output = (
+        <button className="button--reset" onClick={this.handleIconClick}>
+          <Icon icon={icon} />
+        </button>
+      );
+    } else if (avatar) {
+      output = (
+        <Avatar
+          size="small"
+          name={avatar.name}
+          type={avatar.type}
+          image={avatar.image}
+        />
+      );
+    }
+
+    if (output) {
+      return (
+        <div className="resource__column-left__intro__media">
+          {output}
+        </div>
+      );
+    }
+
+    return false;
+  }
+
+  renderTitle = () => {
+    const { title, titleSub, children } = this.props;
 
     return (
+      <div className="resource__column-left__intro__title">
+        {title}{titleSub && <span className="resource__column-left__intro__title__sub">{titleSub}</span>}
+        {children && <div className="resource__column-left__intro__title__meta">{children}</div>}
+      </div>
+    );
+  }
+
+  render() {
+    return (
       <div className="resource__column-left__intro">
-        {title}
+        {this.renderMedia()}
+        {this.renderTitle()}
       </div>
     );
   }
@@ -20,11 +70,23 @@ class ResourceColumnLeftIntro extends React.Component {
 ResourceColumnLeftIntro.displayName = 'RhinoResourceColumnLeftIntro';
 
 ResourceColumnLeftIntro.propTypes = {
+  avatar: PropTypes.shape({
+    image: PropTypes.string,
+    name: PropTypes.string,
+    type: PropTypes.oneOf(['default', 'member']),
+  }),
+  children: PropTypes.node,
   title: PropTypes.string,
+  titleSub: PropTypes.string,
+  icon: PropTypes.string,
+  iconClick: PropTypes.func,
 };
 
 ResourceColumnLeftIntro.defaultProps = {
   title: '',
+  titleSub: '',
+  icon: '',
+  iconClick: () => {},
 };
 
 export default ResourceColumnLeftIntro;
