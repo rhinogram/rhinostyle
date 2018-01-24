47f885ab01dbb4685f56f8dea50ab57a5c266276
Zach Schnackel <zachschnackel@gmail.com>
2018-01-24 10:14:24 -0500

    resource fixes

--- src/scripts/components/ResourceColumnRightWrapper.jsx
+++ src/scripts/components/ResourceColumnRightWrapper.jsx
@@ -1,11 +1,15 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import cx from 'classnames';
 
 const ResourceColumnRightWrapper = (props) => {
-  const { children } = props;
+  const { children, noFlex } = props;
+  const classes = cx('resource__column-right-wrapper', {
+    'resource__column-right-wrapper--no-flex': noFlex,
+  });
 
   return (
-    <div className="resource__column-right-wrapper">
+    <div className={classes}>
       {children}
     </div>
   );
@@ -15,6 +19,7 @@ ResourceColumnRightWrapper.displayName = 'RhinoResourceColumnRightWrapper';
 
 ResourceColumnRightWrapper.propTypes = {
   children: PropTypes.node,
+  noFlex: PropTypes.bool,
 };
 
 export default ResourceColumnRightWrapper;
