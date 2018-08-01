7bdaaf386e65332363c19b371e9699fe83e9e756
Zach Schnackel <info@zslabs.com>
2018-08-01 14:18:24 -0400

    Resource split (resource bottom) (#121)
    * Work on splitting up resource component to allow for "bottom" area
    
    * Resolve psuedo selector visibility altering dom on hover/focus
    
    * make sure min-height centers content on wrapper
    
    * resourcebottom component
    
    * switch out false for null in children check

--- src/scripts/components/Resource.jsx
+++ src/scripts/components/Resource.jsx
@@ -2,9 +2,43 @@ import cx from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import { ResourceRight, UtilitySystem } from '../components';
+import { ResourceBottom, ResourceRight, UtilitySystem } from '../components';
 
 class Resource extends React.Component {
+  getChildren = () => {
+    let returnChild = null;
+    const { children } = this.props;
+
+    return React.Children.map(children, (child) => {
+      if (!child) return null;
+
+      if (child.type === ResourceBottom) {
+        returnChild = null;
+      } else {
+        returnChild = child;
+      }
+
+      return returnChild;
+    });
+  }
+
+  getResourceBottomChildren = () => {
+    let returnChild = null;
+    const { children } = this.props;
+
+    return React.Children.map(children, (child) => {
+      if (!child) return null;
+
+      if (child.type === ResourceBottom) {
+        returnChild = child;
+      } else {
+        returnChild = null;
+      }
+
+      return returnChild;
+    });
+  }
+
   hasRightColumn = () => {
     const { children } = this.props;
     let rightColumnInstance = false;
@@ -27,22 +61,28 @@ class Resource extends React.Component {
   }
 
   render() {
-    const { className, children, active, disabled, selected, interfaceMode, unread } = this.props;
+    const { className, active, disabled, selected, interfaceMode, unread, wrapperClassName } = this.props;
 
     const interfaceClass = interfaceMode === 'radio' ? 'radio' : 'checkbox';
 
-    const classes = cx('resource', className, {
+    const wrapperClasses = cx('resource__wrapper', wrapperClassName, {
       [UtilitySystem.config.classes.active]: active && !interfaceMode,
       'has-interface': interfaceMode,
       [`is-${interfaceClass}`]: interfaceMode,
       'is-selected': selected && !active,
       'is-unread': unread,
+    });
+
+    const resourceClasses = cx('resource', className, {
       'has-right-column': this.hasRightColumn(),
     });
 
     return (
-      <div role="button" tabIndex={0} className={classes} onClick={this.handleClick} disabled={disabled}>
-        {children}
+      <div role="button" tabIndex={0} className={wrapperClasses} onClick={this.handleClick} disabled={disabled}>
+        <div className={resourceClasses}>
+          {this.getChildren()}
+        </div>
+        {this.getResourceBottomChildren()}
       </div>
     );
   }
@@ -57,6 +97,7 @@ Resource.propTypes = {
   interfaceMode: PropTypes.oneOf(['radio', 'checkbox']),
   onClick: PropTypes.func,
   unread: PropTypes.bool,
+  wrapperClassName: PropTypes.string,
 };
 
 export default Resource;
