cc20083d72b30d2ff1e7abd05a259ae28562380c
Andrew London <london.andrewd@gmail.com>
2018-10-10 10:32:42 -0400

    added dist scripts

--- config/watch.js
+++ config/watch.js
@@ -12,7 +12,7 @@ const { reload } = browserSync;
 export default function watch() {
   gulp.watch(paths.svg.src, ['icons', reload]);
   gulp.watch(paths.styles.docAll, ['dist:styles', 'docs:styles', reload]);
-  gulp.watch(paths.scripts.src, ['docs:scripts', reload]);
+  gulp.watch(paths.scripts.src, ['dist:scripts', 'docs:scripts', reload]);
   gulp.watch([paths.metalsmith.pages, paths.metalsmith.templates], ['docs:site', reload]);
   // Stop old version of gulp watch from running when modified
   gulp.watch(['gulpfile.babel.js', './config/*']).on('change', () => process.exit(0));
