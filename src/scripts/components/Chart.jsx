c1a5f9f0b3343da056feebbfeb9ae17321380554
rosh-ttn <roshni.kashyap@tothenew.com>
2018-10-31 16:30:41 +0530

    BEM naming convention and updates in Charts component functions

--- src/scripts/components/Chart.jsx
+++ src/scripts/components/Chart.jsx
@@ -6,101 +6,46 @@ import { Bar, Doughnut, Line } from 'react-chartjs-2';
 import { Icon } from '../components';
 import Tooltip from './Tooltip';
 
-function renderBar(opts) {
-  return (
-    <div className="chart-wrapper">
-      <div className="chart-header u-flex">
-        <div className="chart-title">
-          {opts.title}
-          {opts.info && (
-            <Tooltip content={opts.info}>
-              <Icon className="info-icon" icon="info-circle" />
-            </Tooltip>
-          )}
-        </div>
-        {opts.header && opts.header.text && (
-          <div className={`chart-subtitle u-flex u-flex-align-items-baseline ${opts.header.color}`}>
-            {opts.header.text}
-            {opts.subHeader && (
-              <div className="subtitle-info">
-                {opts.subHeader}
-              </div>
-            )}
-          </div>
-        )}
-      </div>
-      <Bar {...opts} />
-    </div>
-  );
-}
-
-function renderLine(opts) {
-  return (
-    <div className="chart-wrapper">
-      <div className="chart-header u-flex">
-        <div className="chart-title">
-          {opts.title}
-          {opts.info && (
-            <Tooltip content={opts.info}>
-              <Icon className="info-icon" icon="info-circle" />
-            </Tooltip>
-          )}
-        </div>
-        {opts.header && opts.header.text && (
-          <div className={`chart-subtitle u-flex u-flex-align-items-baseline ${opts.header.color}`}>
-            {opts.header.text}
-            {opts.subHeader && (
-              <div className="subtitle-info">
-                {opts.subHeader}
-              </div>
-            )}
-          </div>
-        )}
-      </div>
-      <Line {...opts} />
-    </div>
-  );
-}
-
-function renderDoughnut(opts) {
-  return (
-    <div className="chart-wrapper">
-      <div className="chart-header u-flex">
-        <div className="chart-title">
-          {opts.title}
-          {opts.info && (
-            <Tooltip content={opts.info}>
-              <Icon className="info-icon" icon="info-circle" />
-            </Tooltip>
-          )}
-        </div>
-        {opts.header && opts.header.text && (
-          <div className={`chart-subtitle u-flex u-flex-align-items-baseline ${opts.header.color}`}>
-            {opts.header.text}
-            {opts.subHeader && (
-              <div className="subtitle-info">
-                {opts.subHeader}
-              </div>
-            )}
-          </div>
-        )}
-      </div>
-      <Doughnut {...opts} />
-    </div>
-  );
-}
-const Chart = (props) => {
-  const { ...opts } = props;
+const renderChart = (opts) => {
   switch (opts.type.toLowerCase()) {
     case 'bar':
-      return renderBar(opts);
+      return (<Bar {...opts} />);
     case 'doughnut':
-      return renderDoughnut(opts);
+      return (<Doughnut {...opts} />);
     case 'line':
-      return renderLine(opts);
+      return (<Line {...opts} />);
     default:
-      return renderBar(opts);
+      return (<Bar {...opts} />);
   }
+};
+
+const Chart = (props) => {
+  const { ...opts } = props;
+  return (
+    <div className="chart">
+      <div className="chart__header u-flex">
+        <div className="header__title">
+          {opts.title}
+          {opts.info && (
+            <Tooltip content={opts.info}>
+              <Icon className="header__icon--info" icon="info-circle" />
+            </Tooltip>
+          )}
+        </div>
+        {opts.header && opts.header.text && (
+          <div className={`header__subtitle ${opts.header.color}`}>
+            {opts.header.text}
+            {opts.subHeader && (
+              <span className="subtitle--muted">
+                {opts.subHeader}
+              </span>
+            )}
+          </div>
+        )}
+      </div>
+      {renderChart(opts)}
+    </div>
+  );
 };
 
 
