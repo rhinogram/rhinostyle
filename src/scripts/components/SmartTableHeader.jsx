a6d8595c45ff5e13c99cb4a742b6fb577196d2a8
hardy12994 <hardeepsingh12994@gmail.com>
2019-07-12 17:21:06 +0530

    #rhin-2893, Adding SmartTableHeader in Rhinostyle.

--- src/scripts/components/SmartTableHeader.jsx
+++ src/scripts/components/SmartTableHeader.jsx
@@ -4,59 +4,29 @@ import {
   Icon,
 } from '.';
 
-class SmartTableHeader extends React.Component {
-  state = {
-    sortKey: this.props.sortKey,
-    headers: this.props.headers,
-  };
-  static activeCaretClassName = 'u-text-body';
+const SmartTableHeader = (props) => {
+  const activeCaretClassName = 'u-text-body';
+  const { headerName, ascending, isActive, sortKey, customHeaderSort } = props;
+  const caretUpClass = isActive && ascending === 1 ? activeCaretClassName : ''; // ascending
+  const caretDownClass = isActive && ascending === -1 ? activeCaretClassName : ''; // descending
 
-  headerHandler = (selectedHeader) => {
-    const { headers } = this.state;
-    const currentDirection = this.state.headers[selectedHeader].direction;
-    const lastActiveHeader = Object.keys(headers).find(header => (headers[header].default === true));
-
-    Object.keys(headers).forEach((key) => {
-      headers[key].direction = -1;
-      headers[key].default = false;
-    });
-
-    headers[selectedHeader].default = true;
-    if (lastActiveHeader === selectedHeader) {
-      headers[selectedHeader].direction = currentDirection * -1;
-    } else {
-      headers[selectedHeader].direction = 1;
-    }
-    return headers;
-  };
-
-  customHeaderSort = (sortKey) => {
-    const headers = this.headerHandler(sortKey);
-    this.setState({ sortKey, headers });
-  }
-
-  render() {
-    const { sortKey, headers } = this.state;
-    const { activeCaretClassName } = SmartTableHeader;
-    const caretUpClass = headers[sortKey].default && headers[sortKey].direction === 1 ? activeCaretClassName : ''; // ascending
-    const caretDownClass = headers[sortKey].default && headers[sortKey].direction === -1 ? activeCaretClassName : ''; // descending
-
-    return (
-      <div className="u-flex u-p-a-small" onClick={() => this.customHeaderSort(sortKey)}>
-        {this.props.headerName}
-        <span className="u-flex u-flex-direction-column">
-          <Icon icon="caret-up" className={caretUpClass} />
-          <Icon icon="caret-down" className={caretDownClass} />
-        </span>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="u-flex u-p-a-small" onClick={() => customHeaderSort(sortKey)}>
+      {headerName}
+      <span className="u-flex u-flex-direction-column">
+        <Icon icon="caret-up" className={caretUpClass} />
+        <Icon icon="caret-down" className={caretDownClass} />
+      </span>
+    </div>
+  );
+};
 
 SmartTableHeader.propTypes = {
-  headerName: PropTypes.string.isRequired,
-  sortKey: PropTypes.string.isRequired,
-  headers: PropTypes.object.isRequired,
+  headerName: PropTypes.string,
+  sortKey: PropTypes.string,
+  ascending: PropTypes.number,
+  customHeaderSort: PropTypes.func,
+  isActive: PropTypes.bool,
 };
 
 export default SmartTableHeader;
