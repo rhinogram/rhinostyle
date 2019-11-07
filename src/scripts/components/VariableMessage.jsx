ada3c1c1820264a24758500e17872bd4b8f1cf0a
hardy12994 <hardeepsingh12994@gmail.com>
2019-11-07 20:15:45 +0530

    changes for scroll in safari on touch

--- src/scripts/components/VariableMessage.jsx
+++ src/scripts/components/VariableMessage.jsx
@@ -28,10 +28,13 @@ class VariableMessage extends React.Component {
     categories: [],
     selectedCategory: this.props.defaultSelectedCategory || '',
     precedingChar: '',
+    isSafariBrowser: false,
   };
 
   componentDidMount() {
     this.handleCursorSet();
+    this.checkForSafariBrowser();
+
     const toUpdateState = {
       categories: [],
       variablesOfCategory: [],
@@ -58,6 +61,49 @@ class VariableMessage extends React.Component {
       this.setState({
         message: this.props.initialValue,
       }, this.handleInitValue);
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.state.isSafariBrowser) {
+      window.removeEventListener('touchmove', (event) => {
+        const editorDivId = `variable-message-input-${this.id}`;
+
+        if (event.target.id === editorDivId) {
+          this.compose.focus();
+        } else {
+          this.compose.blur();
+        }
+      });
+    }
+  }
+
+  checkForSafariBrowser = () => {
+    const ua = navigator.userAgent.toLowerCase();
+    let isSafariBrowser = false;
+    if (ua.indexOf('safari') !== -1) {
+      if (ua.indexOf('chrome') > -1) {
+        // Chrome
+        isSafariBrowser = false;
+      } else {
+        // Safari
+        isSafariBrowser = true;
+      }
+    }
+    this.setState({ isSafariBrowser }, () => this.addScrollEventForSafari());
+  }
+
+  addScrollEventForSafari() {
+    if (this.state.isSafariBrowser) {
+      window.addEventListener('touchmove', (event) => {
+        const editorDivId = `variable-message-input-${this.id}`;
+
+        if (event.target.id === editorDivId) {
+          this.compose.focus();
+        } else {
+          this.compose.blur();
+        }
+      });
     }
   }
 
