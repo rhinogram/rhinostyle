59da4e748b6d8255dd2e4f68be30b462f7db261a
Juan Fabrega <juanantoniofabrega@gmail.com>
2019-11-19 10:09:24 -0500

    Adding events and finalizing docs.

--- src/scripts/components/Collapse.jsx
+++ src/scripts/components/Collapse.jsx
@@ -1,34 +1,80 @@
-import cx from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const Collapse = (props) => {
-  const { isOpen, className, children } = props;
+class Collapse extends React.Component {
+  state = {
+    collapseContainerHeight: '0px',
+    isTransitioning: false,
+  };
 
-  const classes = cx('collapse', className, {
-    'collapse--open': isOpen,
-    'collapse--closed': !isOpen,
-  });
+  componentDidMount() {
+    const { isOpen } = this.props;
 
-  return (
-    <div className={classes}>
-      {isOpen && (
-        <div className="collapse__children-wrapper">
-          {children}
-        </div>
-      )}
-    </div>
-  );
-};
+    this.setState({ collapseContainerHeight: isOpen ? this.collapseContainerRef.current.scrollHeight : '0px' });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { isOpen } = this.props;
+
+    if (isOpen !== prevProps.isOpen) {
+      if (prevState.isTransitioning) {
+        this.handleTransitionCancelled();
+      }
+
+      this.setState({ isTransitioning: true }, () => { // eslint-disable-line react/no-did-update-set-state
+        this.handleTransitionStart();
+        this.setState({ collapseContainerHeight: isOpen ? this.collapseContainerRef.current.scrollHeight : '0px' });
+      });
+    }
+  }
+
+  collapseContainerRef = React.createRef();
+
+  // There is no synthetic event available for a transition start.
+  // We have to simulated it by triggering an event when the user changes the 'isOpen' prop.
+  handleTransitionStart = () => {
+    if (this.props.onTransitionStart) {
+      this.props.onTransitionStart();
+    }
+  };
+
+  handleTransitionEnd = (event) => {
+    if (this.props.onTransitionEnd) {
+      this.props.onTransitionEnd(event);
+    }
+    this.setState({ isTransitioning: false });
+  };
+
+  // Fires when a Collapse is toggled mid-transition, since it means the 'transitionEnd' event will never fire.
+  handleTransitionCancelled = () => {
+    if (this.props.onTransitionCancelled) {
+      this.props.onTransitionCancelled();
+    }
+  }
+
+  render() {
+    const { children } = this.props;
+
+    return (
+      <div
+        className={`collapse__container ${this.props.className}`}
+        ref={this.collapseContainerRef}
+        onTransitionEnd={this.handleTransitionEnd}
+        style={{ height: this.state.collapseContainerHeight }}
+      >
+        {children}
+      </div>
+    );
+  }
+}
 
 Collapse.propTypes = {
-  isOpen: PropTypes.bool,
+  children: PropTypes.node.isRequired,
   className: PropTypes.string,
-  children: PropTypes.node,
-};
-
-Collapse.defaultProps = {
-  isOpen: true,
+  isOpen: PropTypes.bool.isRequired,
+  onTransitionStart: PropTypes.func,
+  onTransitionEnd: PropTypes.func,
+  onTransitionCancelled: PropTypes.func,
 };
 
 export default Collapse;
--- src/scripts/docs/CollapseApp.jsx
+++ src/scripts/docs/CollapseApp.jsx
@@ -1,16 +1,44 @@
 import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 
-import { Collapse, Button } from '../components';
+import {
+  Collapse,
+  Button,
+  Bucket,
+  BucketHeader,
+  BucketBody,
+  Icon,
+  Scrollbars,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+} from '../components';
 import { Live } from './components';
 import CollapseExample from './examples/Collapse.example.txt';
 
-const CollapseDocs = {};
+const CollapseDocs = {
+  children: 'A single node containing the contents inside the collapsed panel.',
+  className: 'Class passed to the collapse container',
+  isOpen: 'Control the state of the collapse',
+  onTransitionStart: 'Optional function that will be run when the transition begins',
+  onTransitionEnd: 'Optional function that will be run when the transition ends',
+  onTransitionCancelled: 'Optional function that will be run if the collapse is toggled mid-transition, this is helpful when wanting to reverse anything done at onTransitionStart event since we will not see a onTransitionEnd here.', // eslint-disable-line max-len
+};
 const CollapseScope = {
   React,
   ReactDOM,
   Collapse,
   Button,
+  Bucket,
+  BucketHeader,
+  BucketBody,
+  Icon,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Scrollbars,
 };
 
 const CollapseApp = () => (
