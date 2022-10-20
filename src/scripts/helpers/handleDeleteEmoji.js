672d1fe60fa01bd9a64ae85eb7050b2f2085724b
mvboettcher <mvboettcher@gmail.com>
2022-10-20 11:22:35 -0400

    updates comments

--- src/scripts/helpers/handleDeleteEmoji.js
+++ src/scripts/helpers/handleDeleteEmoji.js
@@ -9,8 +9,10 @@ function isEmoji(str) {
 // a grapheme is a representation of a character as one visual unit (e.g. "🏳️‍🌈" instead of "1f3f3-fe0f-200d-1f308")
 const splitter = new GraphemeSplitter();
 
+// this function was added to resolve an issue with the way in which Firefox handles the deletion of emojis.
+// when hitting backspace on an emoji in Firefox, it takes multiple keypresses to delete a "combined" emoji (emoji with zero-width-joiners)
+// e.g. 🏳️‍🌈 is recognized as 🏳️🌈 and therefore requires two backspaces to delete
 function handleDeleteEmoji(event, inputRef, inputName, message, handleChange) {
-  // only necessary for Firefox
   const isFirefox = navigator.userAgent.includes('Firefox');
   // if backspace is pressed, get the grapheme directly behind the cursor (character to be deleted)
   if (isFirefox && event.code === 'Backspace') {
