d57094a7cb1fda3955de54d4cd743df238cfa4b0
Abhishek Srivastava <abhicool957@gmail.com>
2025-03-12 19:46:55 +0530

    RHIN-11171 add notification settings icon update

--- src/scripts/components/Icon.jsx
+++ src/scripts/components/Icon.jsx
@@ -24,7 +24,7 @@ class Icon extends React.Component {
           xmlns="http://www.w3.org/2000/svg"
           width="24"
           height="24"
-          viewBox="0 0 26 26"
+          viewBox="0 0 24 24"
           fill="transparent"
           strokeLinecap="round"
           strokeLinejoin="round"
